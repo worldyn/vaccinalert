c1594f032f2936473080744ddefc2cfb8118a54b
Adam J <adamjac@kth.se>
2021-04-07 23:07:15 +0200

    WIP: scraping, extracted img src

--- vaccinalert/scrape.py
+++ vaccinalert/scrape.py
@@ -8,31 +8,19 @@ browser = webdriver.PhantomJS()
 url = 'https://www.1177.se/Stockholm/sjukdomar--besvar/lungor-och-luftvagar/inflammation-och-infektion-ilungor-och-luftror/om-covid-19--coronavirus/om-vaccin-mot-covid-19/boka-tid-for-vaccination-mot-covid-19-i-stockholms-lan/'
 browser.get(url)
 
-print("=> \n\nScraping 1177.se")
+print("\n\n=> Scraping 1177.se")
 #print(browser.title)
-boxes = browser.find_elements_by_class_name("c-teaser-outer")
+
+boxes = browser.find_elements_by_css_selector(".c-teaser-outer .c-image img")
+
 #with open("1177.html", "w") as f:
 #    f.write(browser.page_source)
 
 num_boxes = len(boxes)
 print("=> Number of boxes: ", num_boxes)
 
+for i,box in enumerate(boxes):
+    print("IMG SRC: ", box.get_attribute("src"))
+
 
 browser.quit()
-
-
-
-#response = requests.get(url) #Getting the response from mentioned URL using get() method of requests 
-#html = response.content
-#soup = BeautifulSoup(html)  
-#print(soup)
-
-'''
-table = soup.find('table', attrs={'id': 'example2'}) #From BeautifulSoup of HTML content, finding the tbody(data of table) of the desired table having specific attributes, here desired table has 'example2' as idtbody = table.find('tbody')
-list_of_rows = []
-for row in tbody.findAll('tr')[0:]:  #line 11-16, Traversing every row('tr') and every cell of a row ('td') in table and making list of rows    list_of_cells = []
-    for cell in row.findAll('td'):
-        text = cell.text.replace(' ', '')
-        list_of_cells.append(text)
-    list_of_rows.append(list_of_cells)
-'''
