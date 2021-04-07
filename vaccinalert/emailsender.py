37ac9cf86745347c523824042f682c193ea824df
GustafHolmer <gustaf.holmer@outlook.com>
2021-04-07 23:52:50 +0200

    feature: send emails

--- vaccinalert/emailsender.py
+++ vaccinalert/emailsender.py
@@ -1,24 +1,27 @@
 # Import smtplib for the actual sending function
 import smtplib
 import dbutils 
-
+import json
 
 message = """\
-Subject: Hi there
+Subject: 1177 vaccination page changed in your region
 
-This message is sent from Python."""
+This mail was sent to you because you had signed up for the vaccine alert mail list. Please check the 
+1177 vaccination page as it has been updated.\n Regards,\nwww.vaccinalert.se"""
 
 
 def send(receivers):
+
     # creates SMTP session
     s = smtplib.SMTP('smtp.gmail.com', 587)
     
     # start TLS for security
     s.starttls()
     
-
+    # open json file with mails 
     f = open('gmailaccountnames.json', 'r')
     data = json.load(f)
+    sender_email = data["sender_email"]
 
     # Authentication
     s.login(data["sender_email"], data["sender_psw"])
@@ -43,4 +46,4 @@ def send_emails(selected_region):
 
 
 # test
-send_emails("stockholm")
+#send_emails("stockholm")
