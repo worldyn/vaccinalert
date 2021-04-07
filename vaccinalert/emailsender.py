bee04428f15de41574bb8bcc4169d318444accfd
GustafHolmer <gustaf.holmer@outlook.com>
2021-04-07 22:33:06 +0200

    WI

--- vaccinalert/emailsender.py
+++ vaccinalert/emailsender.py
@@ -2,26 +2,34 @@
 import smtplib
 
 sender_email = "vaccinalert1@gmail.com"
-receiver_email = "gustafholmeri@gmail.com"
+
+# take from database
+receiver_emails = ["gustafholmeri@gmail.com", "gustaf.holmer@outlook.com"] 
+
 message = """\
 Subject: Hi there
 
 This message is sent from Python."""
 
 
-# creates SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
-  
-# start TLS for security
-s.starttls()
-  
-# Authentication
-s.login(sender_email, "weqnu5-myjmaf-jYsqom")
-  
+def send_emails():
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    
+    # start TLS for security
+    s.starttls()
+    
+    # Authentication
+    s.login(sender_email, "weqnu5-myjmaf-jYsqom")
+    
 
-  
-# sending the mail
-s.sendmail(sender_email, receiver_email, message)
-  
-# terminating the session
-s.quit()
+    
+    # sending the mail
+    for email in receiver_emails:
+        s.sendmail(sender_email, email, message)
+    
+    # terminating the session
+    s.quit()
+
+
+send_emails()
