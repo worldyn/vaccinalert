1f2a43270a9009ba2cb8b82dfa2b60f126a6191c
Adam J <adamjac@kth.se>
2021-04-09 20:28:23 +0200

    input checking main form

--- vaccinalert/__init__.py
+++ vaccinalert/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 import json
+import re
 
 app = Flask(__name__)
 
@@ -20,12 +21,28 @@ def index():
     if request.method == "POST": # TODO fix clean form fields
         details = request.form
         email = details['email']
-        region = details['region']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO Users(email, region, verified) VALUES (%s, %s, FALSE)", (email, region))
-        mysql.connection.commit()
-        cur.close()
-        return render_template('success.html')
+        regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$' # email regex
+        if not re.search(regex, email):
+            print("not an email ")
+            return render_template('error.html')
+        #region = details['region']
+        region = "stockholm"
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT * FROM Users WHERE email = %s", (email,))
+            res = cur.fetchall()
+            if len(res) > 0: # email exists
+                #print("email exists")
+                return render_template('error.html')
+
+            cur.execute("INSERT INTO Users(email, region, verified) VALUES (%s, %s, FALSE)", (email, region))
+            mysql.connection.commit()
+            cur.close()
+            # send email verification
+            return render_template('success.html')
+        except:
+            print("server error...")
+            return render_template('error.html')
     return render_template('index.html')
 
 
