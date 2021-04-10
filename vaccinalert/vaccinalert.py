5589408ecbed2f927d0e47b7b1c49c0946e786ed
Adam J <adamjac@kth.se>
2021-04-10 02:29:33 +0200

    uwsgi solved, fixed templates

--- vaccinalert/vaccinalert.py
+++ vaccinalert/vaccinalert.py
@@ -19,6 +19,7 @@ mysql = MySQL(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST": # TODO fix clean form fields
+        print("POST")
         details = request.form
         email = details['email']
         regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$' # email regex
@@ -28,21 +29,29 @@ def index():
         #region = details['region']
         region = "stockholm"
         try:
+            print("trying connection")
             cur = mysql.connection.cursor()
+            print(" executing..")
             cur.execute("SELECT * FROM Users WHERE email = %s", (email,))
+            print(" fetching all")
             res = cur.fetchall()
             if len(res) > 0: # email exists
-                #print("email exists")
+                print("email exists")
+                cur.close()
                 return render_template('error.html')
+            print("email not exist, execute insert")
 
             cur.execute("INSERT INTO Users(email, region, verified) VALUES (%s, %s, FALSE)", (email, region))
+            print("commit")
             mysql.connection.commit()
             cur.close()
             # send email verification
             return render_template('success.html')
         except:
-            print("server error...")
+            print("server error mysql...")
+            cur.close()
             return render_template('error.html')
+    print("getting...")
     return render_template('testindex.html')
 
 if __name__ == "__main__":
