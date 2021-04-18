70dcba4df55f4d715ed3fea962b0e47461c94cd5
GustafHolmer <gustaf.holmer@outlook.com>
2021-04-18 14:40:54 +0200

    :

--- vaccinalert/vaccinalert.py
+++ vaccinalert/vaccinalert.py
@@ -54,4 +54,7 @@ def index():
     return render_template('testindex.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+    app.run(debug=True, host='0.0.0.0')
