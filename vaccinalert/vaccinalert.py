bf80b1e8c4a844373cf2b9433c62e35078165e09
GustafHolmer <gustaf.holmer@outlook.com>
2021-04-21 15:34:04 +0200

    changed code for images

--- vaccinalert/vaccinalert.py
+++ vaccinalert/vaccinalert.py
@@ -53,12 +53,7 @@ def index():
     print("getting...")
     return render_template('testindex.html')
 
-@app.after_request
-def add_header(response):
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Expires'] = '0'
-    return response
+
 
 
 if __name__ == "__main__":
